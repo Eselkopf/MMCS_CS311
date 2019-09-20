bb433444cc2816cbe140ff82e726a6917c47a20e
unknown <user@mil8a-202a-02.mmcs.sfedu.ru>
2019-09-20 20:50:01 +0300

    Implement SimpleComparitions

--- Module2/SimpleLangLexer/SimpleLexer.cs
+++ Module2/SimpleLangLexer/SimpleLexer.cs
@@ -156,55 +156,106 @@ namespace SimpleLexer
             LexText = "";
             LexRow = row;
             LexCol = col;
-            // Тип лексемы определяется по ее первому символу
-            // Для каждой лексемы строится синтаксическая диаграмма
-            if (currentCh == ';')
-            {
-                NextCh();
-                LexKind = Tok.SEMICOLON;
-            }
-            else if (currentCh == ':')
-            {
-                NextCh();
-                if (currentCh != '=')
-                {
-                    LexError("= was expected");
-                }
-                NextCh();
-                LexKind = Tok.ASSIGN;
-            }
-            else if (char.IsLetter(currentCh))
-            {
-                while (char.IsLetterOrDigit(currentCh))
-                {
-                    NextCh();
-                }
-                if (keywordsMap.ContainsKey(LexText))
-                {
-                    LexKind = keywordsMap[LexText];
-                }
-                else
-                {
-                    LexKind = Tok.ID;
-                }
-            }
-            else if (char.IsDigit(currentCh))
-            {
-                while (char.IsDigit(currentCh))
-                {
-                    NextCh();
-                }
-                LexValue = Int32.Parse(LexText);
-                LexKind = Tok.INUM;
-            }
-            else if ((int)currentCh == 0)
-            {
-                LexKind = Tok.EOF;
-            }
-            else
-            {
-                LexError("Incorrect symbol " + currentCh);
-            }
+			// Тип лексемы определяется по ее первому символу
+			// Для каждой лексемы строится синтаксическая диаграмма
+			if (currentCh == ';')
+			{
+				NextCh();
+				LexKind = Tok.SEMICOLON;
+			}
+			else if (currentCh == ':')
+			{
+				NextCh();
+				if (currentCh != '=')
+				{
+					LexError("= was expected");
+				}
+				NextCh();
+				LexKind = Tok.ASSIGN;
+			}
+			else if (currentCh == '+')
+			{
+				NextCh();
+				if (currentCh == '=')
+				{
+					NextCh();
+					LexKind = Tok.PLUSASSIGN;
+				}
+				else
+				{
+					LexKind = Tok.PLUS;
+				}
+			}
+			else if (currentCh == '=')
+			{
+				NextCh();
+				LexKind = Tok.EQ;
+			}
+			else if (currentCh == '>')
+			{
+				NextCh();
+				if (currentCh == '=')
+				{
+					NextCh();
+					LexKind = Tok.GEQ;
+				}
+				else
+				{
+					LexKind = Tok.GT;
+				}
+			}
+			else if (currentCh == '<')
+			{
+				NextCh();
+				if (currentCh == '=')
+				{
+					NextCh();
+					LexKind = Tok.LEQ;
+				}
+				else if (currentCh == '>')
+				{
+					NextCh();
+					LexKind = Tok.NEQ;
+				}
+				else
+				{
+					LexKind = Tok.LT;
+				}
+			}
+			else if (char.IsLetter(currentCh))
+			{
+				while (char.IsLetterOrDigit(currentCh))
+				{
+					NextCh();
+				}
+				if (keywordsMap.ContainsKey(LexText))
+				{
+					LexKind = keywordsMap[LexText];
+				}
+				else
+				{
+					LexKind = Tok.ID;
+				}
+			}
+			else if (char.IsDigit(currentCh))
+			{
+				while (char.IsDigit(currentCh))
+				{
+					NextCh();
+				}
+				LexValue = Int32.Parse(LexText);
+				LexKind = Tok.INUM;
+			}
+			else if ((int)currentCh == 0)
+			{
+				LexKind = Tok.EOF;
+			}
+			else
+			{
+				LexError("Incorrect symbol " + currentCh);
+			}
+
+
         }
 
         public virtual void ParseToConsole()
